419ac29cf650d8198cbd6217a5db73a6f6616878
Ivan Nodyanosh <111501255+IvaNodyanosh@users.noreply.github.com>
2024-04-05 01:10:03 +0300

    complyte task

---
+++ controllers/contactsControllers.js
@@ -0,0 +1,80 @@
+import { listContacts } from "../services/contactsServices.js";
+import { getContactById } from "../services/contactsServices.js";
+import { removeContact } from "../services/contactsServices.js";
+import { addContact } from "../services/contactsServices.js";
+import { renovationContact } from "../services/contactsServices.js";
+import HttpError from "../helpers/HttpError.js";
+import { createContactSchema } from "../schemas/contactsSchemas.js";
+import { updateContactSchema } from "../schemas/contactsSchemas.js";
+
+export const getAllContacts = async (__, res, next) => {
+  try {
+    const contacts = await listContacts();
+    res.status(200).json(contacts);
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const getOneContact = async (req, res, next) => {
+  try {
+    const { id } = req.params;
+    const contact = await getContactById(id);
+    if (!contact) {
+      throw HttpError(404);
+    }
+    res.status(200).json(contact);
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const deleteContact = async (req, res, next) => {
+  try {
+    const { id } = req.params;
+    const contact = await removeContact(id);
+    if (!contact) {
+      throw HttpError(404);
+    }
+    res.status(200).json(contact);
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const createContact = async (req, res, next) => {
+  try {
+    const { error } = createContactSchema.validate(req.body);
+    if (error) {
+      throw HttpError(400, error.message);
+    }
+    const contact = await addContact(req.body);
+    res.status(201).json(contact);
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const updateContact = async (req, res, next) => {
+  try {
+    if (req.body) {
+      throw HttpError(400, "Body must have at least one field");
+    }
+    const { id } = req.params;
+    const contact = await renovationContact(id, req.body);
+
+    if (!contact) {
+      throw HttpError(404);
+    }
+
+    const { error } = updateContactSchema.validate(req.body);
+
+    if (error) {
+      throw HttpError(400, error.message);
+    }
+
+    res.status(201).json(contact);
+  } catch (error) {
+    next(error);
+  }
+};
